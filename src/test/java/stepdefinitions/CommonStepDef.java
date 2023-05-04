cb520abb24d34dc69e3ea666642c675d14715398
Fethi <fethiguneyy@gmail.com>
2023-05-04 22:08:09 +0300

    04/052

--- src/test/java/stepdefinitions/CommonStepDef.java
+++ src/test/java/stepdefinitions/CommonStepDef.java
@@ -2,7 +2,7 @@ package stepdefinitions;
 
 import io.cucumber.java.en.*;
 
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 
 public class CommonStepDef {
 
@@ -11,5 +11,25 @@ public class CommonStepDef {
         open(string);
     }
 
+    @Then("user waits {int} seconds")
+    public void user_waits_seconds(Integer number) {
+        sleep(number*1000);
+    }
+    @Then("user navigates the back page")
+    public void user_navigates_the_back_page() {
+        back();
+    }
+    @Then("user navigates the forward page")
+    public void user_navigates_the_forward_page() {
+        forward();
+    }
+    @Then("user refresh the page")
+    public void user_refresh_the_page() {
+        refresh();
+    }
+    @Then("user keeps the page open")
+    public void user_keeps_the_page_open() {
+
+    }
 
 }
--- src/test/resources/features/01_navigation.feature
+++ src/test/resources/features/01_navigation.feature
@@ -4,3 +4,9 @@
     @google
     Scenario: Navigations test
       Given user goes to "https://www.google.com.tr/" url
+      Then user goes to "https://www.amazon.com" url
+      And user waits 5 seconds
+      Then user navigates the back page
+      And user navigates the forward page
+      Then user refresh the page
+      And user keeps the page open
